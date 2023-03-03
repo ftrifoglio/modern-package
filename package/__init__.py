65f38a2a04010f567556bb79fc499e637b0edb65
Federico Trifoglio <federico.trifoglio@assemblyglobal.com>
2023-03-03 21:04:30 +0000

    feat: add subtract function

--- package/__init__.py
+++ package/__init__.py
@@ -1,23 +1,7 @@
 """Package."""
 
+from .math import add, subtract
+
 __version__ = "0.1.0"
 
-
-def add(a: int, b: int) -> int:
-    """
-    Add two numbers.
-
-    Parameters
-    ----------
-    a : int
-        First number.
-    b : int
-        Second number.
-
-    Returns
-    -------
-    int
-        Sum of the two numbers.
-
-    """
-    return a + b
+__all__ = ["add", "subtract"]
