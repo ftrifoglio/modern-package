9cf7a999e8bcc10d8bed9fbeac0b910faea0f74e
Federico Trifoglio <federico.trifoglio@assemblyglobal.com>
2023-03-03 10:00:36 +0000

    feat: add first release of package

---
+++ package/__init__.py
@@ -0,0 +1,23 @@
+"""Package."""
+
+__version__ = "0.0.9"
+
+
+def add(a: int, b: int) -> int:
+    """
+    Add two numbers.
+
+    Parameters
+    ----------
+    a : int
+        First number.
+    b : int
+        Second number.
+
+    Returns
+    -------
+    int
+        Sum of the two numbers.
+
+    """
+    return a + b
