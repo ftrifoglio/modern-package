61c362c8ba84ef76790adf0f63d3c17563e8f46d
Federico Trifoglio <federico.trifoglio@assemblyglobal.com>
2023-03-06 11:11:14 +0000

    fix: fix typing hints of math module

--- package/math.py
+++ package/math.py
@@ -1,76 +1,78 @@
 """Math functions."""
 
+from __future__ import annotations
+
 from functools import reduce
 
 
-def add(*nums: int) -> int:
+def add(*nums: int | float) -> int | float:
     """
     Add all numbers.
 
     Parameters
     ----------
-    nums : int
+    nums : int or float
         Numbers to add. Comma separated.
 
     Returns
     -------
-    int
+    int or float
         Sum of all numbers.
 
     """
     return reduce(lambda a, b: a + b, list(nums))
 
 
-def subtract(*nums: int) -> int:
+def subtract(*nums: int | float) -> int | float:
     """
     Subtract all numbers.
 
     Parameters
     ----------
-    nums : int
+    nums : int or float
         Numbers to subtract. Comma separated.
 
     Returns
     -------
-    int
+    int or float
         Subtraction of all numbers.
 
     """
     return reduce(lambda a, b: a - b, list(nums))
 
 
-def multiply(*nums: int) -> int:
+def multiply(*nums: int | float) -> int | float:
     """
     Multiply all numbers.
 
     Parameters
     ----------
-    nums : int
+    nums : int or float
         Numbers to multiply. Comma separated.
 
     Returns
     -------
-    int
+    int or float
         Multiplication of all numbers.
 
     """
     return reduce(lambda a, b: a * b, list(nums))
 
 
-def exponentiate(base: int, exponent: int) -> int:
+def exponentiate(base: int | float, exponent: int | float) -> int | float:
     """
     Calculate power of a number.
 
     Parameters
     ----------
-    base : int
+    base : int or float
         Base number.
-    exponent : int
+    exponent : int or float
         Exponent.
 
     Returns
     -------
-    int
+    int or float
         Power of a number.
 
     """
